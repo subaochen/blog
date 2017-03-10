3d1a8d51a07a917eea8749fe14f2f82f63db1c4f
subaochen <subaochen@126.com>
2017-03-10 17:38:57 +0800

    new posts

---
+++ src/java/OpCodeTest.java
@@ -0,0 +1,14 @@
+public class OpCodeTest {
+    public int test() {
+        int x;
+        try {
+            x = 1;
+            return x;
+        } catch (Exception e) {
+            x = 2;
+            return x;
+        } finally {
+            x = 3;
+        }
+    }
+}
