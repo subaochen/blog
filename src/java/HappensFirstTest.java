730734f304177411ecda06ed494ccc5279d8f7ad
Su Baochen <subaochen@126.com>
2017-04-13 18:25:44 +0800

    new post

---
+++ src/java/HappensFirstTest.java
@@ -0,0 +1,22 @@
+/**
+ * Created by subaochen on 17-3-29.
+ */
+public class HappensFirstTest {
+
+    static class Seed {
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        private int value;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/linux/install-btsync.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "deb http://linux-packages.resilio.com/resilio-sync/deb resilio-sync non-free" | sudo tee /etc/apt/sources.list.d/resilio-sync.list
+wget -qO - https://linux-packages.resilio.com/resilio-sync/key.asc | sudo apt-key add -
+sudo apt-get -y update
+sudo apt-get -y install resilio-sync
+sudo systemctl enable resilio-sync
