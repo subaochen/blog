30bf61b78206f439c124f38a02010a4fb1ae550a
su baochen <subaochen@126.com>
2017-05-05 07:16:06 +0800

    redis benchmark的测试数据尺寸产生策略：步长递进

--- src/redis-benchmark.sh
+++ src/redis-benchmark.sh
@@ -5,8 +5,9 @@ KEYSPACE=100000
 TOTAL_REQUESTS=1000000
 # pipeline数量，如果不使用pipeline则留空
 PIPELINES=8
+FILE_BENCHMARK=redis_benchmark.dat
 # 首先清空benchmark数据文件
-cp /dev/null  redis-benchmark.dat
+cp /dev/null $FILE_BENCHMARK
 function benchmark() {
     data_size=$1
     redis-cli  flushall > /dev/null
@@ -15,7 +16,7 @@ function benchmark() {
     fi
     benchmark_cmd="redis-benchmark $with_pipeline -r $KEYSPACE -n $TOTAL_REQUESTS -t set -d $data_size"
     qts=`$benchmark_cmd | grep "requests per second" | awk '{print $1}'`
-    echo "$data_size,$qts" | tee -a redis-benchmark.dat
+    echo "$data_size,$qts" | tee -a $FILE_BENCHMARK
 }
 
 STEP=100
@@ -28,3 +29,17 @@ for i in `seq 5000 $STEP 30000`; do
     benchmark $i;
 done
 
+# 另外一个策略：每次步长增加10%
+FIRST=2
+FINAL=300
+FACTOR=10
+data_size=$FIRST
+while [ $data_size -le $FINAL ]
+do
+    echo $data_size;
+    step=$[$data_size / $FACTOR];
+    if [ $step -le 1 ]; then
+        step=10
+    fi
+    data_size=$[$data_size + $step];
+done
