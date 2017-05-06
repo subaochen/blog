537454e5c36905f0e36e1035ad7c3ec8eef56d7a
Su Baochen <subaochen@126.com>
2017-05-06 17:41:46 +0800

    redis benchmark

--- src/redis-benchmark.sh
+++ src/redis-benchmark.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-# 待测试的redis命令
-CMDS="set"
 # redis服务器所在IP地址
 HOST=127.0.0.1
+# 测试结果文件后缀，用于区分不同的测试情形
+# 常见设置：
+# 100M：表示100M网络状况
+# loopback: 本地回环测试
+# tcpip: 通过tcpip网络
+POSTFIX="loopback-100M"
+#POSTFIX="tcpip-1000M"
+# 当吞吐率小于此数值时中止本轮测试
+TPS_THRESHOULD=5000
+# 待测试的redis命令
+CMDS="set"
 # 同时测试的线程数，默认50
 CLIENTS=50
 # 起始测试数据尺寸
@@ -12,7 +21,7 @@ FIRST=2
 FINAL=30000
 # 步长增长因子，每次增长10%
 FACTOR=10
-# 测试数据序列
+# 测试数据序列，默认为空
 SEQUENCE=""
 # 键空间
 KEYSPACE=100000
@@ -20,14 +29,7 @@ KEYSPACE=100000
 TOTAL_REQUESTS=1000000
 # 测试结果文件
 FILE_BENCHMARK=redis-benchmark
-# 测试结果文件后缀，用于区分不同的测试情形
-# 常见设置：
-# 100M：表示100M网络状况
-# loopback: 本地回环测试
-# tcpip: 通过tcpip网络
-POSTFIX="loopback-100M"
-#POSTFIX="tcpip-1000M"
-
+CURRENT_DATE=`date +%Y%m%d`
 # 产生测试数据序列
 data_size=$FIRST
 while [ $data_size -le $FINAL ]
@@ -42,6 +44,7 @@ done
 
 echo $SEQUENCE
 
+tps=0
 function benchmark() {
     data_size=$1
     pipeline=$2
@@ -52,25 +55,32 @@ function benchmark() {
     fi
     benchmark_cmd="redis-benchmark -h $HOST -c $CLIENTS  $with_pipeline -r $KEYSPACE -n $TOTAL_REQUESTS -t $CMDS -d $data_size"
     echo $benchmark_cmd
-    qts=`$benchmark_cmd | grep "requests per second" | awk '{print $1}'`
-    echo "$data_size,$qts" | tee -a $bench_file
+    tps=`$benchmark_cmd | grep "requests per second" | awk '{print $1}'`
+    echo "$data_size,$tps" | tee -a $bench_file
 }
 
 # 不使用pipeline情形
 # 首先清空benchmark数据文件
-bench_file=${FILE_BENCHMARK}-${POSTFIX}.dat
+bench_file=${FILE_BENCHMARK}-${POSTFIX}-${CURRENT_DATE}.dat
 cp /dev/null $bench_file
 for data_size in $SEQUENCE; do
-    benchmark $data_size "" $bench_file;
+    benchmark $data_size "" $bench_file
+    echo $tps
+    if [ ${tps%%.*} -le $TPS_THRESHOULD ]; then
+        break;
+    fi
 done
 
 # 开启pipeline情形
 # 这里只测试8..64时的情形
 # TODO 耗时太长，似乎应该减少测试的轮次数
 for pipeline in {8,16,32,64}; do
-    bench_file=${FILE_BENCHMARK}-${POSTFIX}-P${pipeline}.dat
+    bench_file=${FILE_BENCHMARK}-${POSTFIX}-P${pipeline}-${CURRENT_DATE}.dat
     cp /dev/null $bench_file
     for data_size in $SEQUENCE; do
         benchmark $data_size $pipeline $bench_file
+        if [ ${tps%%.*} -le $TPS_THRESHOULD ]; then
+            break;
+        fi
     done
 done
