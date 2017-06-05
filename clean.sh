09845410c1976861f6260220e840d9f63472358a
Su Baochen <subaochen@126.com>
2017-06-05 17:54:37 +0800

    inkscape to remove spaces from pdf file tex par control sequence summary

--- clean.sh
+++ clean.sh
@@ -4,3 +4,6 @@
 find ./ -name "*.lyx~" -exec rm {} \;
 find ./ -name "*474.lyx" -exec rm {} \;
 find ./ -name "#*.*#" -exec rm {} \;
+find ./ -name "*.log" -exec rm {} \;
+find ./ -name "*.aux" -exec rm {} \;
+find ./ -name "*.dvi" -exec rm {} \;
