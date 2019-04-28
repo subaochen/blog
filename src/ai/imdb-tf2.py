06b6f6e9072ee024b3bdf6a1bc8925794c2502e5
subaochen <subaochen@126.com>
2019-04-28 14:17:15 +0800

    some tensorflow jupyter scripts

---
+++ src/ai/imdb-tf2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+
+print(tf.__version__)
+
+imdb = keras.datasets.imdb
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+print(train_data.shape)
